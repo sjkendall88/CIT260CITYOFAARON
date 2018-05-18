e7b28cbfb2c8d28fb4ef644eb8a31b312b468dfd
sjkendall88 <sjkendall88@byui.edu>
2018-05-18 00:21:56 -0500

    Create Player bean

--- City Of Aaron/src/byui260/aaron/model/Player.java
+++ City Of Aaron/src/byui260/aaron/model/Player.java
@@ -5,10 +5,59 @@
  */
 package byui260.aaron.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author hotja
  */
-public class Player {
+public class Player implements Serializable {
+    // Class instance variables
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
