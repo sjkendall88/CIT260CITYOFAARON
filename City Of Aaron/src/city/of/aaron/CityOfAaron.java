8e6475c144a01fc8377655e6b2a4284efacd62df
sjkendall88 <sjkendall88@byui.edu>
2018-05-18 00:10:17 -0500

    Edit comment section for project

--- City Of Aaron/src/city/of/aaron/CityOfAaron.java
+++ City Of Aaron/src/city/of/aaron/CityOfAaron.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The main () class file to the cityOfAaron project.
+ * CIT-260
+ * Spring 2018
+ * Team members: JJ Hugh, James Rasmussen, and Sterling Kendall.
  */
 package city.of.aaron;
 
