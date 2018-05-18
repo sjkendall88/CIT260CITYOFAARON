77a39d5d25ee4df8395cebb0c90f3d28b0c76fd1
sjkendall88 <sjkendall88@byui.edu>
2018-05-18 01:00:37 -0500

    create teamMember bean

---
+++ City Of Aaron/src/byui260/aaron/model/TeamMember.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui260.aaron.model;
+
+/**
+ *
+ * @author hotja
+ */
+public enum TeamMember {
+    ;
+      
+    private String memberName;
+    private String memberTitle;
+
+    private TeamMember(String memberName, String memberTitle) {
+        this.memberName = memberName;
+        this.memberTitle = memberTitle;
+    }
+    
+    
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberTitle() {
+        return memberTitle;
+    }
+
+    public void setMemberTitle(String memberTitle) {
+        this.memberTitle = memberTitle;
+    }
+    
+    
+    
+}
