3b95ca64ee187ca167e4647190a8606e939494de
sjkendall88 <sjkendall88@byui.edu>
2018-05-18 17:25:50 -0500

    Add bean Location Edit others

---
+++ City Of Aaron/src/byui260/aaron/model/Location.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui260.aaron.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author hotja
+ */
+public class Location implements Serializable{
+    
+    private String description;
+    private String symbol;
+
+    public Location(String description, String symbol) {
+        this.description = description;
+        this.symbol = symbol;
+    }
+    
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    
+    
+    
+}
