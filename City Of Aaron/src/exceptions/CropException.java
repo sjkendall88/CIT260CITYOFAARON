85de63cdbd7102017523b87faa33dd491766ab8b
User <User@PygmyBoy-1AP4200>
2018-07-03 08:04:50 -0600

    Merge origin/master
    Conflicts:
    	City Of Aaron/src/byui/aaron/view/PrintListView.java
    	City Of Aaron/src/byui260/aaron/control/GameControl.java
    	City Of Aaron/src/byui260/aaron/model/ListItem.java

---
+++ City Of Aaron/src/exceptions/CropException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author User
+ */
+public class CropException extends Exception
+{
+    //Default constructor
+    // Purpose: Initializes data members to default values
+    // Parameters: none
+    // Returns: none
+    public CropException() {
+    }
+    
+    //Parameterized  constructor
+    // Purpose: Initializes data members to value passed as a parameter
+    // Parameters: A string containing the error message
+    // Returns: none
+    public CropException(String string)
+    {
+    super(string);
+    } 
+}
